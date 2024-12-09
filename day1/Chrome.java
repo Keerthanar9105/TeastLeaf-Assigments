2d481b37ab873ce1fabd006cbc3e8b99a25b1d6a
Keerthanar9105 <keerthanar9105@gmail.com>
2024-12-09 09:56:07 +0300

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		float browserVersion = 91.0f;
+		String developer = "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("browser version is "+browserVersion);
+		System.out.println("browser developed by "+ developer);
+		System.out.println("browser isBeta version "+isBeta);
+		System.out.println("browser release year is "+releaseYear);
+		System.out.println("browser shortcutkey is "+shortcutKey);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		
+		System.out.println("browser version is " +browserVersion);
+		System.out.println("browser name is " +browserName);
+		System.out.println("bowser is visible " +isVisible);
+		System.out.println("browser release year is "+releaseYear);
+		System.out.println("browser logo is "+browserLogo);
+		
+		
+
+	}
+
+}
